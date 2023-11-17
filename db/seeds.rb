9390cdb57b2403347ca11d63a4069acaab58ef3f
Vira <v.zbitnieva@gmail.com>
2023-11-17 09:23:05 +0200

    added FFaker gem, created by user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Role.destroy_all
 User.destroy_all
+Role.destroy_all
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -11,11 +11,23 @@ User.destroy_all
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-Role.create!(name: 'Користувач', slug: :default)
-admin_role = Role.create!(name: 'Адміністратор', slug: 'admin')
-
-email = 'admin@example.com'
-User.create! email:,
-             password: email,
+# ------------------Roles-------------
+default_role = Role.create!(name: 'Користувач', slug: :default)
+admin_role = Role.create!(name: 'Адміністратор', slug: :admin)
+# --------------Admin user-------------
+admin_email = 'admin@example.com'
+User.create! email: admin_email,
+             password: admin_email,
              role: admin_role
+# -----------Users--------------------
+hash_users = Array.new(15) do
+  email = FFaker::Internet.safe_email
+  {
+    email:,
+    password: email,
+    full_name: FFaker::NameUA.name,
+    role: default_role
+  }
+end
+
+User.create! hash_users
