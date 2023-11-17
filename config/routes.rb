9451c59b9a091c07517419426dea5c51321dfd6b
Vira <v.zbitnieva@gmail.com>
2023-11-17 08:36:42 +0200

    a controller for administration was created

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 #
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   root 'home#index'
